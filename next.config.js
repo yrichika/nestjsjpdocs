1bce4d49796d03fae6efd70b70f47fb5a511e5ff
yrichika <68413973+yrichika@users.noreply.github.com>
2023-10-24 19:36:59 +0900

    プロジェクト初期設定

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'standalone', // necessary to create docker image
+}
 
 module.exports = nextConfig
