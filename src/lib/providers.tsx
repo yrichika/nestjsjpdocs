4a932b16b29df0ed04e987322b62f409e8b736ac
yrichika <68413973+yrichika@users.noreply.github.com>
2023-11-17 08:51:17 +0900

    ビルドエラー、App Routerエラーを解消

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -4,13 +4,10 @@
 import { Provider } from 'react-redux'
 
 /* Instruments */
-import { persistor, reduxStore, rootId } from '@/lib/redux'
+import { persistor, reduxStore } from '@/lib/redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
 export const Providers = (props: React.PropsWithChildren) => {
-  // redux-persist用: これがないとエラーになる
-  document.getElementById(rootId) // FIXME: ビルドでエラーが出る
-
   return (
     <Provider store={reduxStore}>
       <PersistGate loading={null} persistor={persistor}>
